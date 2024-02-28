7a97fe87d0d0920d5141aa6b02002ef92f1c3ab6
= <=>
2024-02-28 17:41:48 +0000

    Update

--- index.js
+++ index.js
@@ -8,8 +8,6 @@ document.getElementById("mobile_menu").addEventListener("click", function () {
   document.getElementById("sidebar").classList.remove("hide_sidebar");
 });
 
-// console.log(document);
-
 document.addEventListener("DOMContentLoaded", function () {
   var mainNavs = document.getElementsByClassName("mainNavItem");
   for (var i = 0; i < mainNavs.length; i++) {
