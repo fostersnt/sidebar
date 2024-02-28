b43dac98f4aa3b50e5f3f13d64842177bd85f9ac
= <=>
2024-02-28 09:32:24 +0000

    Added more logic and styles

--- index.js
+++ index.js
@@ -1,20 +1,42 @@
+/* SideBar Closure */
+document.getElementById("close_sidebar").addEventListener("click", function () {
+  document.getElementById("sidebar").classList.toggle("hide_sidebar");
+});
+
+document.getElementById("mobile_menu").addEventListener("click", function () {
+  document.getElementById("sidebar").classList.toggle("hide_sidebar");
+});
+
 document.addEventListener("DOMContentLoaded", function () {
   var mainNavs = document.getElementsByClassName("mainNavItem");
   for (var i = 0; i < mainNavs.length; i++) {
-    // console.log(mainNavs[i]);
-    // mainNavs[i].classList.remove("active", "hideItem");
     mainNavs[i].addEventListener("click", function () {
+      // for (let index = 0; index < mainNavs.length; index++) {
+      //   let check =
+      //     mainNavs[index].nextElementSibling.classList.contains("hideItem");
+      //   if (!check) {
+      //     mainNavs[index].nextElementSibling.classList.add("hideItem");
+      //     check = false;
+      //   }
+      // }
       var itemToHide = this.nextElementSibling;
-
-      for (let index = 0; index < mainNavs.length; index++) {
-        mainNavs[index].classList.remove("active");
-        mainNavs[index].nextElementSibling.classList.add("hideItem");
-        // continue;
-      }
       this.classList.toggle("active");
-      if (itemToHide) {
-        itemToHide.classList.toggle("hideItem");
-      }
+      itemToHide.classList.toggle("hideItem");
+      // var itemToHide = this.nextElementSibling;
+      // this.classList.toggle("active");
+      // itemToHide.classList.toggle("hideItem");
+      // let check = itemToHide.classList.contains("hideItem");
+      // console.log(itemToHide);
+      // console.log(check);
+      // if (check) {
+      //   itemToHide.classList.remove("hideItem");
+      //   check = false;
+      //   console.log("PRESENT");
+      // } else {
+      //   console.log("MISSING");
+      //   itemToHide.classList.add("hideItem");
+      //   check = true;
+      // }
     });
   }
 });
